051fd59a27d80ad850ad63e7cfa1e6b4c247885b
Gabriel Silva <gabriel.silva@eurekalabs.com.br>
2021-07-24 11:00:46 -0300

    Create users page

---
+++ src/pages/users/index.tsx
@@ -0,0 +1,89 @@
+import {
+  Box,
+  Button,
+  Checkbox,
+  Flex,
+  Heading,
+  Icon,
+  Table,
+  Tbody,
+  Td,
+  Text,
+  Th,
+  Thead,
+  Tr,
+} from '@chakra-ui/react'
+import { RiAddLine, RiPencilLine } from 'react-icons/ri'
+
+import { Header } from '../../components/Header'
+import { Pagination } from '../../components/Pagination'
+import { Sidebar } from '../../components/Sidebar'
+
+export default function UserList() {
+  return (
+    <Box>
+      <Header />
+
+      <Flex w='100%' my='6' maxWidth={1480} mx='auto' px='6'>
+        <Sidebar />
+
+        <Box flex='1' borderRadius={8} bg='gray.800' p='8'>
+          <Flex mb='8' justify='space-between' align='center'>
+            <Heading size='lg' fontWeight='normal'>
+              Usuários
+            </Heading>
+            <Button
+              as='a'
+              size='sm'
+              fontSize='sm'
+              colorScheme='pink'
+              leftIcon={<Icon as={RiAddLine} fontSize='20' />}
+            >
+              Criar Novo
+            </Button>
+          </Flex>
+          <Table colorScheme='whiteAlpha'>
+            <Thead>
+              <Tr>
+                <Th px='6' color='gray.300' width='8'>
+                  <Checkbox colorScheme='pink' />
+                </Th>
+                <Th>Usuário</Th>
+                <Th>Data de Cadastro</Th>
+                <Th width='8'></Th>
+              </Tr>
+            </Thead>
+            <Tbody>
+              <Tr>
+                <Td px='6'>
+                  <Checkbox colorScheme='pink' />
+                </Td>
+                <Td>
+                  <Box>
+                    <Text fontWeight='bold'>Gabriel Silva</Text>
+                    <Text fontSize='sm' color='gray.300'>
+                      emailbrabo@gmail.com
+                    </Text>
+                  </Box>
+                </Td>
+                <Td>04 de Julho, 2021</Td>
+                <Td>
+                  <Button
+                    as='a'
+                    size='sm'
+                    fontSize='sm'
+                    colorScheme='gray.900'
+                    leftIcon={<Icon as={RiPencilLine} fontSize='16' />}
+                  >
+                    Editar
+                  </Button>
+                </Td>
+              </Tr>
+            </Tbody>
+          </Table>
+          <Pagination />
+        </Box>
+      </Flex>
+    </Box>
+  )
+}
