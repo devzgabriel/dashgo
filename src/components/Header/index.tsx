cc3c6e64cfb8c8b91066ff70e238052034616814
Gabriel Silva <gabriel.silva@eurekalabs.com.br>
2021-07-27 21:37:29 -0300

    new components

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,34 @@
+import { Flex, useBreakpointValue } from '@chakra-ui/react'
+import { Logo } from './Logo'
+import { NotificationsNav } from './NotificationsNav'
+import { Profile } from './Profile'
+import { Search } from './Search'
+
+export function Header() {
+  const isWideVersion = useBreakpointValue({
+    base: false,
+    lg: true,
+  })
+
+  return (
+    <Flex
+      as='header'
+      w='100%'
+      maxWidth={1480}
+      h='auto'
+      mx='auto'
+      mt='4'
+      px='6'
+      align='center'
+    >
+      <Logo />
+
+      {isWideVersion && <Search />}
+
+      <Flex align='center' ml='auto'>
+        <NotificationsNav />
+        <Profile showProfileData={isWideVersion} />
+      </Flex>
+    </Flex>
+  )
+}
